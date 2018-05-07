dcdd252abc70a25d731bb92fdc5839dc4f073d20
Tornhoof <tornhoof@gmail.com>
2018-05-07 11:12:00 +0200

    Forgot one

--- SpanJson.Benchmarks/Program.cs
+++ SpanJson.Benchmarks/Program.cs
@@ -7,8 +7,6 @@ namespace SpanJson.Benchmarks
     {
         private static void Main(string[] args)
         {
-            var sb = new SelectedBenchmarks();
-            sb.ReadStringUtf8();
             BenchmarkRunner.Run<SelectedBenchmarks>();
         }
     }
