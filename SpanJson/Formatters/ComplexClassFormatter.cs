919d4a0ff5504199f425208c5d482dddd325286b
Tornhoof <tornhoof@gmail.com>
2018-06-05 11:16:31 +0200

    initial tests working

--- SpanJson/Formatters/ComplexClassFormatter.cs
+++ SpanJson/Formatters/ComplexClassFormatter.cs
@@ -3,7 +3,7 @@
     /// <summary>
     ///     Used for types which are not built-in
     /// </summary>
-    public sealed class ComplexClassFormatter<T, TSymbol, TResolver> : ComplexFormatter<T, TSymbol, TResolver>
+    public sealed class ComplexClassFormatter<T, TSymbol, TResolver> : ComplexFormatter<T, TSymbol, TResolver>, IJsonFormatter<T, TSymbol, TResolver>
         where T : class where TResolver : IJsonFormatterResolver<TSymbol, TResolver>, new() where TSymbol : struct
     {
         public static readonly ComplexClassFormatter<T, TSymbol, TResolver> Default = new ComplexClassFormatter<T, TSymbol, TResolver>();
