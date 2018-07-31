8c946bd41f65bbe34a8a67936541f6c709f34892
Tornhoof <Tornhoof@users.noreply.github.com>
2018-07-31 22:38:04 +0200

    Two Dimensional Array support (#55)
    Two Dimensional Array Support

---
+++ SpanJson.Tests/ArrayTests.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Xunit;
+
+namespace SpanJson.Tests
+{
+    public class ArrayTests
+    {
+
+        [Fact]
+        public void JaggedArrayUtf16()
+        {
+            var input = new int[5][] {new[] {1, 2}, new[] {3, 4}, new[] {5, 6, 7}, new[] {7, 8}, new[] {9, 10}};
+            var serialized = JsonSerializer.Generic.Utf16.Serialize(input);
+            Assert.NotNull(serialized);
+            var deserialized = JsonSerializer.Generic.Utf16.Deserialize<int[][]>(serialized);
+            Assert.NotNull(deserialized);
+            Assert.Equal(input, deserialized);
+        }
+
+        [Fact]
+        public void JaggedArrayUtf8()
+        {
+            var input = new int[5][] {new[] {1, 2}, new[] {3, 4}, new[] {5, 6, 7}, new[] {7, 8}, new[] {9, 10}};
+            var serialized = JsonSerializer.Generic.Utf8.Serialize(input);
+            Assert.NotNull(serialized);
+            var deserialized = JsonSerializer.Generic.Utf8.Deserialize<int[][]>(serialized);
+            Assert.NotNull(deserialized);
+            Assert.Equal(input, deserialized);
+        }
+
+        [Fact]
+        public void Nested2DArrayUtf16()
+        {
+            var input = new int[,] {{1, 2, 3}, {3, 4, 5}, {5, 6, 7}, {7, 8, 9}};
+            var serialized = JsonSerializer.Generic.Utf16.Serialize(input);
+            Assert.NotNull(serialized);
+            Assert.Equal("[[1,2,3],[3,4,5],[5,6,7],[7,8,9]]", serialized);
+            var deserialized = JsonSerializer.Generic.Utf16.Deserialize<int[,]>(serialized);
+            Assert.NotNull(deserialized);
+            Assert.Equal(input, deserialized);
+        }
+
+        [Fact]
+        public void Nested2DArrayEmptyUtf16()
+        {
+            var input = new int[,] {{ }, { }, { }, { }};
+            var serialized = JsonSerializer.Generic.Utf16.Serialize(input);
+            Assert.NotNull(serialized);
+            Assert.Equal("[[],[],[],[]]", serialized);
+            var deserialized = JsonSerializer.Generic.Utf16.Deserialize<int[,]>(serialized);
+            Assert.NotNull(deserialized);
+            Assert.Equal(input, deserialized);
+        }
+
+
+        [Fact]
+        public void Nested2DArrayUtf8()
+        {
+            var input = new int[,] {{1, 2, 3}, {3, 4, 5}, {5, 6, 7}, {7, 8, 9}};
+            var serialized = JsonSerializer.Generic.Utf8.Serialize(input);
+            Assert.NotNull(serialized);
+            var deserialized = JsonSerializer.Generic.Utf8.Deserialize<int[,]>(serialized);
+            Assert.NotNull(deserialized);
+            Assert.Equal(input, deserialized);
+        }
+
+        [Fact]
+        public void Nested3DArrayUtf16()
+        {
+            var input = new int[,,]
+            {
+                {{1, 2, 3}, {4, 5, 6}},
+                {{7, 8, 9}, {10, 11, 12}}
+            };
+            Assert.ThrowsAny<Exception>(() => JsonSerializer.Generic.Utf16.Serialize(input));
+        }
+
+
+
+        [Fact]
+        public void Nested3DArrayUtf8()
+        {
+            var input = new int[,,]
+            {
+                {{1, 2, 3}, {4, 5, 6}},
+                {{7, 8, 9}, {10, 11, 12}}
+            };
+            Assert.ThrowsAny<Exception>(() => JsonSerializer.Generic.Utf8.Serialize(input));
+        }
+
+
+        [Fact]
+        public void JaggedAsNestedArrayUtf16()
+        {
+            var input = new int[5][] {new[] {1, 2}, new[] {3, 4}, new[] {5, 6, 7}, new[] {7, 8}, new[] {9, 10}};
+            var serialized = JsonSerializer.Generic.Utf16.Serialize(input);
+            var ex = Assert.Throws<JsonParserException>(() => JsonSerializer.Generic.Utf16.Deserialize<int[,]>(serialized));
+            Assert.Equal(JsonParserException.ParserError.InvalidArrayFormat, ex.Error);
+        }
+
+    }
+}
---
+++ SpanJson/Formatters/TwoDimensionalArrayFormatter.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SpanJson.Helpers;
+using SpanJson.Resolvers;
+
+namespace SpanJson.Formatters
+{
+    public sealed class TwoDimensionalArrayFormatter<T, TSymbol, TResolver> : BaseFormatter, IJsonFormatter<T[,], TSymbol>
+        where TResolver : IJsonFormatterResolver<TSymbol, TResolver>, new() where TSymbol : struct
+    {
+        public static readonly TwoDimensionalArrayFormatter<T, TSymbol, TResolver> Default = new TwoDimensionalArrayFormatter<T, TSymbol, TResolver>();
+
+        private static readonly IJsonFormatter<T, TSymbol> ElementFormatter =
+            StandardResolvers.GetResolver<TSymbol, TResolver>().GetFormatter<T>();
+
+        public void Serialize(ref JsonWriter<TSymbol> writer, T[,] value, int nestingLimit)
+        {
+            if (value == null)
+            {
+                writer.WriteNull();
+                return;
+            }
+
+            var nextNestingLimit = RecursionCandidate<T>.IsRecursionCandidate ? nestingLimit + 1 : nestingLimit;
+            var firstLength = value.GetLength(0);
+            var secondLength = value.GetLength(1);
+            writer.WriteBeginArray();
+            if (firstLength > 0)
+            {
+                writer.WriteBeginArray();
+                if (secondLength > 0)
+                {
+                    SerializeRuntimeDecisionInternal<T, TSymbol, TResolver>(ref writer, value[0, 0], ElementFormatter, nextNestingLimit);
+                    for (var k = 1; k < secondLength; k++)
+                    {
+                        writer.WriteValueSeparator();
+                        SerializeRuntimeDecisionInternal<T, TSymbol, TResolver>(ref writer, value[0, k], ElementFormatter, nextNestingLimit);
+                    }
+                }
+
+                writer.WriteEndArray();
+                for (var i = 1; i < firstLength; i++)
+                {
+                    writer.WriteValueSeparator();
+                    writer.WriteBeginArray();
+                    if (secondLength > 0)
+                    {
+                        SerializeRuntimeDecisionInternal<T, TSymbol, TResolver>(ref writer, value[i, 0], ElementFormatter, nextNestingLimit);
+                        for (var k = 1; k < secondLength; k++)
+                        {
+                            writer.WriteValueSeparator();
+                            SerializeRuntimeDecisionInternal<T, TSymbol, TResolver>(ref writer, value[i, k], ElementFormatter, nextNestingLimit);
+                        }
+                    }
+
+                    writer.WriteEndArray();
+                }
+            }
+
+            writer.WriteEndArray();
+        }
+
+        public T[,] Deserialize(ref JsonReader<TSymbol> reader)
+        {
+            var values = new List<T[]>(4);
+            reader.ReadBeginArrayOrThrow();
+            var count = 0;
+            while (!reader.TryReadIsEndArrayOrValueSeparator(ref count)) // count is already preincremented, as it counts the separators
+            {
+                values.Add(ArrayFormatter<T, TSymbol, TResolver>.Default.Deserialize(ref reader));
+            }
+
+            if (values.Count == 0)
+            {
+                return new T[0, 0];
+            }
+
+            var length = values[0].Length;
+            if (!values.TrueForAll(a => a.Length == length))
+            {
+                throw new JsonParserException(JsonParserException.ParserError.InvalidArrayFormat, reader.Position);
+            }
+
+            var result = new T[values.Count, length];
+            for (int i = 0; i < values.Count; i++)
+            {
+                for (int j = 0; j < length; j++)
+                {
+                    result[i, j] = values[i][j];
+                }
+            }
+
+            return result;
+        }
+    }
+}
