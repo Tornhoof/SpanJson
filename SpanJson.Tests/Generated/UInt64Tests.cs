a77b14b1ca0e9af7b698e3e37df1685eb2bdd15b
Tornhoof <Tornhoof@gmail.com>
2018-12-08 13:14:46 +0100

    Add manual ulong overflow tests

---
+++ SpanJson.Tests/Generated/UInt64Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SpanJson.Tests.Generated
+{
+    public partial class UInt64Tests
+    {
+
+        [Fact]
+        public void SerializeDeserializeOverflowUtf8()
+        {
+            // ulong.MaxValue +1
+            Assert.Throws<OverflowException>(() => JsonSerializer.Generic.Utf8.Deserialize<UInt64>(Encoding.UTF8.GetBytes("18446744073709551616")));
+        }
+
+        [Fact]
+        public void SerializeDeserializeOverflowUtf16()
+        {
+            // ulong.MaxValue +1
+            Assert.Throws<OverflowException>(() => JsonSerializer.Generic.Utf16.Deserialize<UInt64>("18446744073709551616"));
+        }
+    }
+}
