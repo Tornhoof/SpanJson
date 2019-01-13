95eda912d1192e6665f600cf73826f3b8b45ac5c
Tornhoof <Tornhoof@gmail.com>
2019-01-13 19:08:13 +0100

    Console for jitdump

---
+++ SpanJson.Console/Program.cs
@@ -0,0 +1,28 @@
+﻿using SpanJson.Shared.Models;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace SpanJson.Console
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            var serialized = GetPayload();
+            System.Console.WriteLine(serialized.Length);
+            var deserialized = SpanJson.JsonSerializer.Generic.Utf16.Deserialize<Answer>(serialized);
+        }
+
+        private static string GetPayload()
+        {
+            using (var stream = Assembly.GetAssembly(typeof(Program)).GetManifestResourceStream(typeof(Program), "Payload.json"))
+            {
+                using (var tr = new StreamReader(stream))
+                {
+                    return tr.ReadToEnd();
+                }
+            }
+        }
+    }
+}
