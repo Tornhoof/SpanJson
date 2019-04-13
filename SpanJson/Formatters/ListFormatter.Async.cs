71c99052776d527afc3f32f898514adce7d8359e
Tornhoof <Tornhoof@gmail.com>
2019-04-13 13:31:35 +0200

    let's try with 3.0

--- SpanJson/Formatters/ListFormatter.Async.cs
+++ SpanJson/Formatters/ListFormatter.Async.cs
@@ -10,6 +10,11 @@ namespace SpanJson.Formatters
     {
         public ValueTask SerializeAsync(AsyncJsonWriter<TSymbol> asyncWriter, TList value, CancellationToken cancellationToken = default)
         {
+            // let it run sync until the serialized size hits ~32kb
+            // if a single entity is
+            // flushasync
+            // new jsonwriter, start from pos 0
+
             var writer = asyncWriter.Create();
             if (value == null)
             {
@@ -22,82 +27,13 @@ namespace SpanJson.Formatters
                 writer.IncrementDepth();
             }
 
-            var valueLength = value.Count;
-            writer.WriteBeginArray();
-            if (valueLength > 0)
-            {
-                var vTask = WriteElementAsync(asyncWriter, ref writer, value[0], cancellationToken);
-                if (!vTask.IsCompletedSuccessfully)
-                {
-                    return AwaitTask(vTask, asyncWriter, value, 1, cancellationToken);
-                }
-
-                for (var i = 1; i < valueLength; i++)
-                {
-                    writer.WriteValueSeparator();
-                    vTask = WriteElementAsync(asyncWriter, ref writer, value[i], cancellationToken);
-                    if (!vTask.IsCompletedSuccessfully)
-                    {
-                        return AwaitTask(vTask, asyncWriter, value, i + 1, cancellationToken);
-                    }
-                }
-            }
-
-            if (IsRecursionCandidate)
-            {
-                writer.DecrementDepth();
-            }
-
-            writer.WriteEndArray();
-            return asyncWriter.FlushAsync(cancellationToken);
-        }
-
-        private static async ValueTask AwaitTask(ValueTask vTask, AsyncJsonWriter<TSymbol> asyncWriter, TList value, int index,
-            CancellationToken cancellationToken = default)
-        {
-            await vTask.ConfigureAwait(false);
-            await WriteElementsAsync(asyncWriter, value, index + 1, cancellationToken).ConfigureAwait(false);
-        }
-
-        private static ValueTask WriteElementAsync(AsyncJsonWriter<TSymbol> asyncWriter, ref JsonWriter<TSymbol> writer, T value,
-            CancellationToken cancellationToken = default)
-        {
-            SerializeRuntimeDecisionInternal<T, TSymbol, TResolver>(ref writer, value, ElementFormatter);
-            if (writer.Position > asyncWriter.MaxSafeWriteSize)
-            {
-                return asyncWriter.FlushAsync(cancellationToken);
-            }
-
-            return new ValueTask();
-        }
-
-        private static ValueTask WriteElementsAsync(AsyncJsonWriter<TSymbol> asyncWriter, TList value, int index,
-            CancellationToken cancellationToken = default)
-        {
-            var writer = asyncWriter.Create();
-            var valueLength = value.Count;
-            for (var i = index; i < valueLength; i++)
-            {
-                writer.WriteValueSeparator();
-                var vTask = WriteElementAsync(asyncWriter, ref writer, value[i], cancellationToken);
-                if (!vTask.IsCompletedSuccessfully)
-                {
-                    return AwaitTask(vTask, asyncWriter, value, i + 1, cancellationToken);
-                }
-            }
-
-            if (IsRecursionCandidate)
-            {
-                writer.DecrementDepth();
-            }
-
-            writer.WriteEndArray();
-            return asyncWriter.FlushAsync(cancellationToken);
+            return default;
         }
 
         public ValueTask<TList> DeserializeAsync(AsyncJsonReader<TSymbol> asyncReader, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            // only problem is string, we need to find the end of the string and buffer until we hit it
+            return default;
         }
     }
 }
