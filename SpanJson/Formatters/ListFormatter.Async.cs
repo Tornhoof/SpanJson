589c79cb4b2e507cd129a5c35d9cd8894a9df6d8
Tornhoof <Tornhoof@gmail.com>
2019-04-13 13:55:50 +0200

    next

--- SpanJson/Formatters/ListFormatter.Async.cs
+++ SpanJson/Formatters/ListFormatter.Async.cs
@@ -27,7 +27,71 @@ namespace SpanJson.Formatters
                 writer.IncrementDepth();
             }
 
-            return default;
+            var valueLength = value.Count;
+            writer.WriteBeginArray();
+            if (valueLength > 0)
+            {
+                var vTask = WriteElement(asyncWriter, ref writer, value[0], cancellationToken);
+                if (!vTask.IsCompletedSuccessfully)
+                {
+                    return new ValueTask(vTask.AsTask().ContinueWith((Task t, object o) => WriteElements(asyncWriter, value, 1, cancellationToken).AsTask(),
+                        cancellationToken, cancellationToken));
+                }
+
+                for (var i = 1; i < valueLength; i++)
+                {
+                    writer.WriteValueSeparator();
+                    SerializeRuntimeDecisionInternal<T, TSymbol, TResolver>(ref writer, value[i], ElementFormatter);
+                    if (writer.Position >= asyncWriter.MaxSafeWriteSize)
+                    {
+                        var i1 = i;
+                        var flushTask = asyncWriter.FlushAsync(cancellationToken).AsTask().ContinueWith(
+                            (Task t, object o) => WriteElements(asyncWriter, value, i1 + 1, cancellationToken).AsTask(), cancellationToken, cancellationToken);
+                        return new ValueTask(flushTask);
+                    }
+                }
+            }
+            if (IsRecursionCandidate)
+            {
+                writer.DecrementDepth();
+            }
+            writer.WriteEndArray();
+            return asyncWriter.FlushAsync(cancellationToken);
+
+        }
+
+        private static ValueTask WriteElement(AsyncJsonWriter<TSymbol> asyncWriter,  ref JsonWriter<TSymbol> writer, T value, CancellationToken cancellationToken = default)
+        {
+            SerializeRuntimeDecisionInternal<T, TSymbol, TResolver>(ref writer, value, ElementFormatter);
+            if (writer.Position >= asyncWriter.MaxSafeWriteSize)
+            {
+                return asyncWriter.FlushAsync(cancellationToken);
+            }
+
+            return new ValueTask();
+        }
+
+        private static ValueTask WriteElements(AsyncJsonWriter<TSymbol> asyncWriter, TList value, int index, CancellationToken cancellationToken = default)
+        {
+            var writer = asyncWriter.Create();
+            var valueLength = value.Count;
+            for (var i = index; i < valueLength; i++)
+            {
+                writer.WriteValueSeparator();
+                SerializeRuntimeDecisionInternal<T, TSymbol, TResolver>(ref writer, value[i], ElementFormatter);
+                if (writer.Position >= asyncWriter.MaxSafeWriteSize)
+                {
+                    var i1 = i;
+                    var flushTask = asyncWriter.FlushAsync(cancellationToken).AsTask().ContinueWith((Task t, object  o) => WriteElements(asyncWriter, value, i1 + 1, cancellationToken).AsTask(), cancellationToken, cancellationToken);
+                    return new ValueTask(flushTask);
+                }
+            }
+            if (IsRecursionCandidate)
+            {
+                writer.DecrementDepth();
+            }
+            writer.WriteEndArray();
+            return asyncWriter.FlushAsync(cancellationToken);
         }
 
         public ValueTask<TList> DeserializeAsync(AsyncJsonReader<TSymbol> asyncReader, CancellationToken cancellationToken = default)
