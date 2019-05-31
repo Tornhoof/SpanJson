407786de2b312bd71fde51b8edf6dca2798cd2cf
Tornhoof <Tornhoof@gmail.com>
2019-05-31 20:36:37 +0200

    Updates

--- SpanJson.WebBenchmark/Startup.cs
+++ SpanJson.WebBenchmark/Startup.cs
@@ -19,13 +19,13 @@ namespace SpanJson.WebBenchmark
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvcCore().AddSerializers().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvcCore().AddSerializers().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            app.UseMvc();
+            app.UseRouting();
         }
     }
 }
