d7374487188be09fa985a8f3b33b7d68f1d4e65f
Tornhoof <Tornhoof@gmail.com>
2019-08-24 19:00:49 +0200

    next

--- SpanJson.Benchmarks/Serializers/SystemTextJsonSerializer.cs
+++ SpanJson.Benchmarks/Serializers/SystemTextJsonSerializer.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Encodings.Web;
 using System.Text.Json;
 
 namespace SpanJson.Benchmarks.Serializers
 {
     public class SystemTextJsonSerializer : SerializerBase<byte[]>
     {
-        private static readonly JsonSerializerOptions Options = new JsonSerializerOptions { IgnoreNullValues = true, PropertyNamingPolicy = JsonNamingPolicy.CamelCase, WriteIndented = false };
+        private static readonly JsonSerializerOptions Options = new JsonSerializerOptions
+        {
+            IgnoreNullValues = true, 
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase, 
+            WriteIndented = false,
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+        };
 
         public override byte[] Serialize<TInput>(TInput input)
         {
