ccf81b4fa9426a2122d188411b53949ecce0925e
Tornhoof <Tornhoof@gmail.com>
2020-11-16 09:10:43 +0100

    First Try

---
+++ SpanJson.Tests/RecordTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SpanJson.Tests
+{
+#if NET5_0
+    public class RecordTests
+    {
+        [Fact]
+        public void SerializeDeserializeRecordUtf16()
+        {
+            var person = new PersonRecord("Hello", "World");
+            var serialized = JsonSerializer.Generic.Utf16.Serialize(person);
+            Assert.NotNull(serialized);
+            var deserialized = JsonSerializer.Generic.Utf16.Deserialize(serialized);
+            Assert.NotNull(deserialized);
+            Assert.Equals(person, deserialized);
+        }
+
+        [Fact]
+        public void SerializeDeserializeRecordUtf8()
+        {
+            var person = new PersonRecord("Hello", "World");
+            var serialized = JsonSerializer.Generic.Utf8.Serialize(person);
+            Assert.NotNull(serialized);
+            var deserialized = JsonSerializer.Generic.Utf8.Deserialize(serialized);
+            Assert.NotNull(deserialized);
+            Assert.Equals(person, deserialized);
+        }
+
+
+        public record PersonRecord(string Name, string Title);
+    }
+#endif
+}
